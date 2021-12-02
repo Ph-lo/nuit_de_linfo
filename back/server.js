ababa14be80e6c1afcfe2642ed9ac0d72d60ce5b
mohamadi msa <mohamadi1.msa-abdou@epitech.eu>
2021-12-02 22:21:39 +0100

    a

--- back/server.js
+++ back/server.js
@@ -5,12 +5,24 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let allValue = ["§%µµ"];
+let isdelete = false;
 app.get("/", (req, res) => {
-  res.send("api nuit de l'info");
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("value", (input) => {
+    allValue.map((el, index) => {
+ 
+    });
+    
+    console.log(allValue);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(8000, () => {
