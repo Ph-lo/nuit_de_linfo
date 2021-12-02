914245149dfec98ec8200c8113ee867bb7244ae0
Cédric Thomas <cedric@reyah.eu>
2021-12-02 23:30:57 +0100

    remove node module

--- back/server.js
+++ back/server.js
@@ -1,3 +1,4 @@
+const loremIpsum = require("lorem-ipsum").loremIpsum;
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -5,16 +6,42 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-
 rooms = {};
 matches = {};
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+
+function generateArticle(keyword) {
+    let title = loremIpsum({
+        count: 6,
+        units: "word",
+    }).toLowerCase();
+    const body = loremIpsum({
+        count: 4,
+        units: "sentence",
+    }).toLowerCase();
+    const title_words = title.split(" ");
+    title_words.push(keyword);
+    const body_words = body.split(" ");
+    const randNbr = Math.floor(Math.random() * 4) + 1;
+    for(let i = 0; i < randNbr; i++)
+        body_words.push(keyword);
+    title_words.sort(() => Math.random());
+    body_words.sort(() => Math.random() * 1);
+    return {
+        title: title_words.join(" ") + '.',
+        body: body_words.join(" ") + '.'
+    };    
+}
+
+app.use(express.static(__dirname + '/public'));
 
 io.on("connection", (socket) => {
-  console.log(`user connected ${socker.id}`);
+  console.log(`user connected ${socket.id}`);
   socket.on("select_room", (roomName) => {
+    const articles = [];
+    for (let i = 0; i < 8; i++) {
+      articles.push(generateArticle(roomName));
+    }
+    socket.emit("search_results", JSON.stringify({articles}));
     if (rooms[roomName] !== undefined && rooms[roomName].connected) {
       matches[socket.id] = rooms[roomName];
       matches[rooms[roomName].id] = socket;
@@ -26,7 +53,12 @@ io.on("connection", (socket) => {
     }
   });
   socket.on("disconnect", () => {
-    console.log(`user disconnected ${socker.id}`);
+    if (matches[socket.id]) {
+      matches[socket.id].emit('surrend');
+      delete matches[matches[socket.id].id];
+      delete matches[socket.id];
+    }
+    console.log(`user disconnected ${socket.id}`);
   });
 });
 
